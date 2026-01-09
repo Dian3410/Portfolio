a77ff942730d1670753d937cd6971cffadf74222
Dian3410 <mhddiand@gmail.com>
2026-01-09 13:05:18 +0100

    premier jet

---
+++ index.php
@@ -0,0 +1,131 @@
+<php>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio – Dian Diallo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header class="navbar">
+    <h2 class="logo">M. Dian Diallo</h2>
+    <nav>
+        <a href="#accueil">Accueil</a>
+        <a href="#apropos">À propos</a>
+        <a href="#formation">Formation</a>
+        <a href="#experience">Expérience</a>
+        <a href="#competences">Compétences</a>
+        <a href="#projets">Projets</a>
+        <a href="#contact">Contact</a>
+    </nav>
+</header>
+
+<!-- ACCUEIL -->
+<section id="accueil" class="hero">
+    <h3>Bonjour je suis</h3>
+    <h1>Dian Diallo</h1>
+    <h3>Étudiant en BTS Services Informatiques aux Organisations</h3>
+    <p>
+        
+    </p>
+
+    <div class="buttons">
+        <a href="#projets" class="btn-primary">Voir mes projets</a>
+        <a href="#contact" class="btn-secondary">Me contacter</a>
+    </div>
+</section>
+
+<!-- A PROPOS -->
+<section id="apropos">
+    <h1 class="section-title">À propos</h1>
+    <p class="center-text">
+        Étudiant en BTS Services Informatiques aux Organisations. Je suis
+        curieux, rigoureux et autonome et je suis passionné par les systèmes, réseaux et la cybersécurité.
+        En formation pour devenir administrateur systèmes et réseaux
+        spécialisé en sécurité informatique.
+    </p>
+</section>
+
+<!-- FORMATION -->
+<section id="formation">
+    <h1 class="section-title">Formation</h1>
+
+    <p><strong>2025 – 2027 :</strong> BTS SIO (en cours) – Lycée Fulbert, Chartres</p>
+    <p><strong>2024 – 2025 :</strong> Baccalauréat scientifique – Mention Assez Bien (Sénégal)</p>
+</section>
+
+<!-- EXPERIENCE -->
+<section id="experience">
+    <h1 class="section-title">Expériences</h1>
+
+    <h3>Zentech – Sénégal (2022–2023)</h3>
+    <ul>
+        <li>Installation et configuration Windows</li>
+        <li>Installation Linux</li>
+        <li>Changement RAM et disque</li>
+    </ul>
+
+    <h3>Bénévolat</h3>
+    <p>Ramassage de déchets sur la voie publique</p>
+</section>
+
+<!-- COMPETENCES -->
+<section id="competences">
+    <h1 class="section-title">Compétences</h1>
+
+    <div class="skills-container">
+
+        <!-- SYSTEMES & RESEAUX -->
+        <div class="skill-card">
+            <h2>Systèmes & Réseaux</h2>
+            <ul>
+                <li>Installation Windows / Linux</li>
+                <li>VirtualBox, VMware</li>
+                <li>Réseaux – Cisco Packet Tracer</li>
+                <li>Wireshark</li>
+                
+            </ul>
+        </div>
+
+        <!-- DEVELOPPEMENT -->
+        <div class="skill-card">
+            <h2>Développement & Programmation</h2>
+            <ul>
+                <li>HTML / CSS</li>
+                <li>JavaScript</li>
+                <li>PHP (notions)</li>
+                <li>Python</li>
+                <li>C#</li>
+                <li>MySQL / MariaDB</li>
+                <li>GitHub</li>
+                <li>Pack Office</li>
+            </ul>
+        </div>
+
+    </div>
+</section>
+
+<!-- PROJETS -->
+<section id="projets">
+    <h1 class="section-title">Projets</h1>
+    <p class="center-text">Projets BTS et personnels à venir</p>
+</section>
+
+<!-- CONTACT -->
+<section id="contact">
+    <h1 class="section-title">Contact</h1>
+    <p class="center-text">
+        📧 Mhddiand@gmail.com <br>
+        📞 06 95 04 92 81 <br>
+        📍 Chartres
+    </p>
+</section>
+
+<script src="script.js"></script>
+</body>
+</html>
+
+
+</php>
